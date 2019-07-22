d21a3db94baa5e184355b7408e269b2c67703f1c
Logan Allen <loganallc@gmail.com>
2019-07-22 13:09:59 -0700

    Added all interface files to monorepo

---
+++ pkg/interface/publish/src/js/components/lib/icons/sigil.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { sigil, reactRenderer } from 'urbit-sigil-js';
+
+    
+export class Sigil extends Component {
+  render() {
+    const { props } = this;
+
+    return (
+      <div 
+        className="bg-black" 
+        style={{ flexBasis: 35, padding: 4 }}>
+      {
+        sigil({
+          patp: props.ship,
+          renderer: reactRenderer,
+          size: props.size,
+          colors: ['black', 'white'],
+        })
+      }
+      </div>
+    );
+  }
+}
+
